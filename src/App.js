a935fcc1741b1a587d8768847a5c648c8f2182bf
AneeshaRama <aneesharama@gmail.com>
2022-05-21 10:46:47 +0530

    Initiated the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/Cart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <>
+      <h1>cart page</h1>
+    </>
+  );
+};
+
+export default Cart;
